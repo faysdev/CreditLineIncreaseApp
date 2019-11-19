64c66f490512223c3baf0ee377367e5aa3482791
faysdev <faysdev@gmail.com>
2019-11-18 20:14:44 -0500

    Cleanup of orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+# Create org
 sfdx force:org:create -f config/project-scratch-def.json -a FlowsADK --setdefaultusername -d 14
 
-#add pckg IDs to Idnum
+# Install packages
 sfdx force:package:install --package 04t1U0000066Af4 -w 20
-
 sfdx force:package:install --package 04t1U000005ZS27 -w 20  
 
-
+# Push source
 sfdx force:source:push -f
 
-#sfdx assign permission sets
+# Assign permission sets
 sfdx force:user:permset:assign -n Credit_Line_Increase_Demo
 
+# Execute additional steps
 sfdx force:apex:execute -f config/create-demo-data-setup.apex
 
+# Open org
 sfdx force:org:open
-
